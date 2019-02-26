d30a6543940aed9577183c2fa9df8b636a6536b1
Christopher Lee <lee@foldmountain.com>
2019-02-26 11:46:23 -0500

    rcnn

---
+++ deepmars/models/rcnn.py
@@ -0,0 +1,163 @@
+from .mrcnn.config import Config
+from .mrcnn import utils
+import .mrcnn.model as modellib
+from .mrcnn import visualize
+from .mrcnn.model import log
+import h5py
+import pandas as pd
+
+class CraterConfig(Config):
+    """Configuration for training on the toy shapes dataset.
+    Derives from the base Config class and overrides values specific
+    to the toy shapes dataset.
+    """
+    # Give the configuration a recognizable name
+    NAME = "craters"
+
+    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 4
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 1  # background + 1 shapes
+
+    # Use small images for faster training. Set the limits of the small side
+    # the large side, and that determines the image shape.
+    IMAGE_MIN_DIM = 256
+    IMAGE_MAX_DIM = 256
+
+    # Use smaller anchors because our image and objects are small
+    RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+
+    # Reduce training ROIs per image because the images are small and have
+    # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+    TRAIN_ROIS_PER_IMAGE = 128
+
+    # Use a small epoch since the data is simple
+    STEPS_PER_EPOCH = 4000
+
+    # use small validation steps since the epoch is small
+    VALIDATION_STEPS = 1000
+
+
+class CraterDataset(utils.Dataset):
+    """Load the crater dataset from a pregenerated HDF5 file."""
+    def load_image(self, image_id):
+        pass
+
+
+    def load_mas(self, image_id):
+        pass
+
+
+    def image_reference(self, image_id):
+        pass
+
+
+    def load_shapes(self, indices):
+        self.index = indices
+        self.directory = "/disks/work/lee/DL/MarsCraters/deepmars/data/processed/"
+
+        self.image_file = []
+        self.crater_file = []
+        self.gen_imgs = []
+        self.gen_craters = []
+        self.count = []
+        counter=0
+        for jval, index in enumerate(indices):
+            image_file = self.directory + "sys_images_{:05d}.hdf5".format(index) 
+            crater_file =  self.directory + "sys_craters_{:05d}.hdf5".format(index)
+            gen_imgs =  h5py.File(image_file, 'r')
+            gen_craters = pd.HDFStore(crater_file, 'r')
+            self.gen_imgs.append(gen_imgs)
+            self.add_class("shapes",1,"crater")
+            count = len(gen_imgs["input_images"])
+            self.count.append(count)
+            from tqdm import tqdm, trange
+            gi = gen_imgs["input_images"][:]
+            for i in trange(count):
+                #loop over images and craters, grab crater locations
+                image_name="img_{:05d}".format(i+index)
+                if image_name not in gen_craters:
+                    continue
+                image=None
+                image = np.ones([256,256, 3], dtype=np.uint8)
+                im = gi[i]
+                image[:, :, 0] = im
+                image[:, :, 1] = im
+                image[:, :, 2] = im
+                
+                craters = gen_craters[image_name]
+                craters["Radius (pix)"] = craters["Diameter (pix)"]/2
+                craters = craters[craters["Diameter (pix)"] > 6]
+                craters = craters[["x","y","Radius (pix)"]].values.astype(int)
+                if len(craters) == 0:
+                    continue
+                self.add_image("shapes", image_id=counter, path=jval,
+                               width=256, height=256,
+                               craters=craters, image=image, mask=None, class_ids=None,
+                               image_name=image_name,
+                               shapes=["crater"]*len(craters), image_index=i)
+
+#            gen_craters.close()
+#            gen_imgs.close()
+                
+    def load_image(self, image_id):
+        """Generate an image from the specs of the given image ID.
+        Typically this function loads the image from a file, but
+        in this case it generates the image on the fly from the
+        specs in image_info.
+        """
+        info = self.image_info[image_id]
+        if info["image"] is None:
+            image = np.ones([info['height'], info['width'], 3], dtype=np.uint8)
+            im = self.gen_imgs[info["path"]]["input_images"][info["image_index"]]
+            image[:,:,0] = im
+            image[:,:,1] = im
+            image[:,:,2] = im
+            self.image_info[image_id]["image"] = image
+        else:
+            image = self.image_info[image_id]["image"]
+        return image
+
+    def image_reference(self, image_id):
+        """Return the shapes data of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "shapes":
+            return info["craters"]
+        else:
+            super(self.__class__).image_reference(self, image_id)
+
+    def load_mask(self, image_id):
+        """Generate instance masks for shapes of the given image ID.
+        """
+        info = self.image_info[image_id]
+        if info["mask"] is None:
+            craters = info['craters']
+            count = len(craters)
+            mask = np.zeros([info['height'], info['width'], count], dtype=np.uint8)
+            for i, dims in enumerate(craters):
+                mask[:, :, i:i+1] = self.draw_shape(mask[:, :, i:i+1].copy(),
+                                                "circle", dims, 1)
+            # Handle occlusions
+            occlusion = np.logical_not(mask[:, :, -1]).astype(np.uint8)
+            for i in range(count-2, -1, -1):
+                mask[:, :, i] = mask[:, :, i] * occlusion
+                occlusion = np.logical_and(occlusion, np.logical_not(mask[:, :, i]))
+            # Map class names to class IDs.
+            class_ids = np.array([self.class_names.index(s) for s in info["shapes"]])
+            info["mask"] = mask.astype(np.bool)
+            info["class_ids"] = class_ids.astype(np.int32)
+        else:
+            mask, class_ids = info["mask"], info["class_ids"]
+        return mask, class_ids
+
+    def draw_shape(self, image, shape, dims, color):
+        """Draws a shape from the given specs."""
+        # Get the center x, y and the size s
+        x, y, s = dims
+        if shape == "circle":
+            cv2.circle(image, (x, y), s, color, 2)
+        return image
+
---
+++ deepmars/models/train_rcnn.py
@@ -0,0 +1,197 @@
+from . import rcnn
+import click
+from dotenv import find_dotenv, load_dotenv
+
+
+minrad_ = 5
+maxrad_ = 40
+longlat_thresh2_ = 1.8
+rad_thresh_ = 1.0
+template_thresh_ = 0.5
+target_thresh_ = 0.1
+
+
+@click.group()
+def dl():
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+
+    # not used in this stub but often useful for finding various files
+    project_dir = Path(__file__).resolve().parents[2]
+
+    # find .env automagically by walking up directories until it's found, then
+    # load up the .env entries as environment variables
+    load_dotenv(find_dotenv())
+    import sys
+    sys.path.append(os.getenv("DM_ROOTDIR"))
+    pass
+
+
+def train_rcnn(config, MP):
+
+    # training dataset
+    dataset_train = CraterDataset()
+    dataset_train.load_shapes(MP["train_indices"])
+    dataset_train.prepare()
+
+    # Validation dataset
+    dataset_val = CraterDataset()
+    dataset_val.load_shapes(MP["dev_indices"])
+    dataset_val.prepare()
+
+    # ## Create Model
+    # Create model in training mode
+    model = modellib.MaskRCNN(mode="training",
+                              config=config,
+                              model_dir=MP["save_dir"])
+
+    # Which weights to start with?
+    init_with = "coco"  # imagenet, coco, or last
+
+    if init_with == "imagenet":
+        model.load_weights(model.get_imagenet_weights(), by_name=True)
+    elif init_with == "coco":
+        # Load weights trained on MS COCO, but skip layers that
+        # are different due to the different number of classes
+        # See README for instructions to download the COCO weights
+        model.load_weights(COCO_MODEL_PATH, by_name=True,
+                       exclude=["mrcnn_class_logits", "mrcnn_bbox_fc", 
+                                "mrcnn_bbox", "mrcnn_mask"])
+    elif init_with == "last":
+        # Load the last model you trained and continue training
+        model.load_weights(model.find_last(), by_name=True)
+
+    # Train in two stages:
+    # 1. Only the heads. Here we're freezing all the
+    # backbone layers and training only the randomly
+    # initialized layers (i.e. the ones that we didn't
+    # use pre-trained weights from MS COCO). To train
+    # only the head layers, pass `layers='heads'` to
+    # the `train()` function.
+    #
+    # 2. Fine-tune all layers. For this simple example
+    # it's not necessary, but we're including it to
+    # show the process. Simply pass `layers="all` to
+    # train all layers.
+
+    # Train the head branches
+    # Passing layers="heads" freezes all layers except the head
+    # layers. You can also pass a regular expression to select
+    # which layers to train by name pattern.
+    model.train(dataset_train, dataset_val, 
+                learning_rate=config.LEARNING_RATE, 
+                epochs=4, 
+                layers='heads')
+
+    # Fine tune all layers
+    # Passing layers="all" trains all layers. You can also 
+    # pass a regular expression to select which layers to
+    # train by name pattern.
+    model.train(dataset_train, dataset_val, 
+                learning_rate=config.LEARNING_RATE / 10,
+                epochs=16, 
+                layers="all")
+
+    # Save weights
+    model.keras_model.save_weights(MP["final_save_name"])
+
+
+
+
+@dl.command()
+@click.option("--model", default=None)
+def train_model(model):
+    """Run Convolutional Neural Network Training
+
+    Execute the training of MaskRCNN on
+    images of Mars and binary ring targets.
+    """
+
+    # Model Parameters
+    MP = {}
+
+    # Directory of train/dev/test image and crater hdf5 files.
+    MP['dir'] = os.path.join(os.getenv("DM_ROOTDIR"),'data/processed/')
+
+    # Image width/height, assuming square images.
+    MP['dim'] = 256
+
+    # Batch size: smaller values = less memory but less accurate gradient estimate
+    MP['bs'] = 4
+
+    # Number of training epochs.
+    MP['epochs'] = 50
+
+    # Number of train/valid/test samples, needs to be a multiple of batch size.
+
+    MP['train_indices'] = list(np.arange(162000, 208000, 2000))
+    MP['dev_indices']   = list(np.arange(161000, 206000, 4000))
+    MP['test_indices']  = list(np.arange(163000, 206000, 4000))
+
+    MP['n_train'] = len(MP["train_indices"])*1000
+    MP['n_dev'] = len(MP["dev_indices"])*1000
+    MP['n_test'] = len(MP["test_indices"])*1000
+    print(MP["n_train"], MP["n_dev"], MP["n_test"])
+
+    # Save model (binary flag) and directory.
+    MP['save_models'] = 1
+    MP["calculate_custom_loss"] = False
+    MP['save_dir'] = 'models'
+    MP['final_save_name'] = 'model_rcnn.h5'
+
+    # initial model
+    MP["model"] = model
+
+       
+    # Model Parameters (to potentially iterate over, keep in lists).
+    #df = pd.read_csv("runs.csv")
+    #for na,ty in [("filter_length", int),
+    #                    ("lr", float),
+    #                    ("n_filters", int),
+    #                    ("init", str),
+    #                    ("lambda", float),
+    #                    ("dropout", float)]:
+    #    MP[na] = df[na].astype(ty).values
+    
+    #MP['N_runs'] = len(MP['lambda'])                # Number of runs
+    #MP['filter_length'] = [3]       # Filter length
+#    MP['lr'] = [0.0001]             # Learning rate
+#    MP['n_filters'] = [112]         # Number of filters
+#    MP['init'] = ['he_normal']      # Weight initialization
+#    MP['lambda'] = [1e-6]           # Weight regularization
+#    MP['dropout'] = [0.15]          # Dropout fraction
+    
+    # Iterating over parameters example.
+    #    MP['N_runs'] = 2
+    #    MP['lambda']=[1e-4,1e-4]
+    print(MP)
+    class CraterConfig(Config):
+        # Give the configuration a recognizable name
+        NAME = "craters"
+        # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
+        # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
+        GPU_COUNT = 1
+        IMAGES_PER_GPU = MP["bs"]
+        # Number of classes (including background)
+        NUM_CLASSES = 1 + 1  # background + 1 shapes
+        # Use small images for faster training. Set the limits of the small side
+        # the large side, and that determines the image shape.
+        IMAGE_MIN_DIM = MP["dim"]
+        IMAGE_MAX_DIM = MP["dim"]
+        # Use smaller anchors because our image and objects are small
+        RPN_ANCHOR_SCALES = (8, 16, 32, 64, 128)  # anchor side in pixels
+        # Reduce training ROIs per image because the images are small and have
+        # few objects. Aim to allow ROI sampling to pick 33% positive ROIs.
+        TRAIN_ROIS_PER_IMAGE = 128
+        # Use a small epoch since the data is simple
+        STEPS_PER_EPOCH = MP["n_train"]
+        # use small validation steps since the epoch is small
+        VALIDATION_STEPS = MP["n_dev"]
+
+    config = CraterConfig()
+    train_rcnn(config,MP)
+
+
+
+if __name__ == '__main__':
+    dl()
